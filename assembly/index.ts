be467890d564225fe1652c9f5d22c0dc7f083c4c
sleepy ramen <rllola80@gmail.com>
2022-06-22 12:02:20 +0200

    Use decorators for contract (#3)

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,32 +1,14 @@
-import {methodNumber, caller, valueReceived, usrUnhandledMsg, create, genericAbort, paramsRaw} from "@zondax/fvm-as-sdk/assembly/wrappers";
-import {NO_DATA_BLOCK_ID, DAG_CBOR, USR_UNSPECIFIED} from "@zondax/fvm-as-sdk/assembly/env";
-import {isConstructorCaller} from "@zondax/fvm-as-sdk/assembly/helpers";
-import {State} from "./state";
+// @filecoinfile
+import {ParamsRawResult} from "@zondax/fvm-as-sdk/assembly/env/types"
+import {isConstructorCaller} from "@zondax/fvm-as-sdk/assembly/helpers"
+import {caller} from "@zondax/fvm-as-sdk/assembly/wrappers"
+import {State} from "./state"
 import {RegisterParams} from "./params"
 
-export function invoke(paramsID: u32): u32 {
-  const methodNum = methodNumber()
-
-  switch (u32(methodNum)) {
-    case 1:
-      constructor()
-      break
-
-    case 2:
-      const rawParams = paramsRaw(paramsID)
-      const params = RegisterParams.fromRaw(rawParams.raw)
-      register(params)
-      break
-
-    default:
-      usrUnhandledMsg()
-  }
-
-  return NO_DATA_BLOCK_ID
-}
-
+// @ts-ignore
+@constructor
 // Function executed on create actor
-function constructor(): void {
+function constructor(rawParams: ParamsRawResult): void {
   if( !isConstructorCaller() ) return;
 
   const emptyMap = new Map<String, u64>()
@@ -36,13 +18,20 @@ function constructor(): void {
   return;
 }
 
+// @ts-ignore
+@export_method(2)
 // `register` associate a domain name to a user address
-function register(params: RegisterParams): void {
+function register(rawParams: ParamsRawResult): Uint8Array {
+  const params = RegisterParams.fromRaw(rawParams.raw)
+
   const address = caller()
   const state = State.load()
   state.count += 1
   state.nameRegister.set(params.name, address)
   state.save()
 
-  return
+  const message = "Success"
+  const msg = Uint8Array.wrap(String.UTF8.encode(message))
+
+  return msg
 }
